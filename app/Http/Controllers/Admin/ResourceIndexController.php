9aa662e9d61f331be3c3f798250863542b15c90d
SmartArmeniaBot <mesropyananushavan@gmail.com>
2026-05-12 22:56:45 +0000

    cron: split role permission bundle title

--- app/Http/Controllers/Admin/ResourceIndexController.php
+++ app/Http/Controllers/Admin/ResourceIndexController.php
@@ -3322,6 +3322,11 @@ class ResourceIndexController extends Controller
 
     private function rolesPermissionsPermissionBundleTimelineTitle(Role $selectedRole): string
     {
+        return $this->rolesPermissionsPermissionBundleTimelineTitleSummary($selectedRole);
+    }
+
+    private function rolesPermissionsPermissionBundleTimelineTitleSummary(Role $selectedRole): string
+    {
         return sprintf('%s permission bundle reflected from model state', $selectedRole->name);
     }
 
