b06437183137a302b4d02cd9469018d7b0776e57
SmartArmeniaBot <mesropyananushavan@gmail.com>
2026-05-13 16:27:03 +0000

    cron: inline roles status timeline title

--- app/Http/Controllers/Admin/ResourceIndexController.php
+++ app/Http/Controllers/Admin/ResourceIndexController.php
@@ -3268,11 +3268,6 @@ class ResourceIndexController extends Controller
 
     private function rolesPermissionsStatusTimelineTitle(Role $selectedRole): string
     {
-        return $this->rolesPermissionsStatusTimelineTitleSummary($selectedRole);
-    }
-
-    private function rolesPermissionsStatusTimelineTitleSummary(Role $selectedRole): string
-    {
         return sprintf('%s status reflected from model state', $selectedRole->name);
     }
 
