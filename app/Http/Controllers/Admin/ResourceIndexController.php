1ba9f065ab4a44d71f9e8662af5619a25af0657e
SmartArmeniaBot <mesropyananushavan@gmail.com>
2026-05-13 17:11:38 +0000

    cron: inline roles review note timeline title

--- app/Http/Controllers/Admin/ResourceIndexController.php
+++ app/Http/Controllers/Admin/ResourceIndexController.php
@@ -3290,11 +3290,6 @@ class ResourceIndexController extends Controller
 
     private function rolesPermissionsReviewNoteTimelineTitle(Role $selectedRole): string
     {
-        return $this->rolesPermissionsReviewNoteTimelineTitleSummary($selectedRole);
-    }
-
-    private function rolesPermissionsReviewNoteTimelineTitleSummary(Role $selectedRole): string
-    {
         return sprintf('%s review note reflected from model state', $selectedRole->name);
     }
 
